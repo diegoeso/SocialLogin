11f67bcf9fa416aee40d3f9d6f39528693f17a39
diegoeso <diego.enrique76@gmail.com>
2019-05-07 18:54:41 -0500

    Login With Facebook and Github

---
+++ config/services.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+     */
+    'facebook'  => [
+        'client_id'     => '1854776957883233',
+        'client_secret' => 'ced13c49efc0f1a076cbde3a79faee41',
+        'redirect'      => 'http://facebooklogin.test/callback/facebook',
+    ],
+
+    'github'    => [
+        'client_id'     => '5fe60731ed783172cca3',
+        'client_secret' => 'a0a37a7c5798aad8c974f9d127a817ad0eedde6d',
+        'redirect'      => 'http://facebooklogin.test/callback/github',
+    ],
+
+    'mailgun'   => [
+        'domain'   => env('MAILGUN_DOMAIN'),
+        'secret'   => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark'  => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses'       => [
+        'key'    => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe'    => [
+        'model'   => App\User::class,
+        'key'     => env('STRIPE_KEY'),
+        'secret'  => env('STRIPE_SECRET'),
+        'webhook' => [
+            'secret'    => env('STRIPE_WEBHOOK_SECRET'),
+            'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
+        ],
+    ],
+
+];
